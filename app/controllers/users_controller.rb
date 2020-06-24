b32ef987fb05f395ef1f67aedafb3d26f81586f1
masatoshi.emori <masaemori0513@gmail.com>
2020-06-24 14:34:04 +0000

    不要なファイルとコードを削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,4 @@
 class UsersController < ApplicationController
-  # before_action :require_user_logged_in, only: [:index]
-  
-  def index
-    @users = User.order(id: :desc)
-  end
 
   def new
     @user = User.new
@@ -14,7 +9,6 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      # redirect_to @user
       redirect_to login_path
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,9 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
-  get 'signup', to:'users#new'
+  get 'signup', to: 'users#new'
   
-  # resources :users, only: [:index, :show, :new, :create]
-  
-  resources :users, only: [:index, :new, :create]
-  
+  resources :users, only: [:new, :create]
   
   resources :tasks
   
